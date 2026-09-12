10b1180fdf1468a350d8ea1b78ed745776f748e1
Chris Barwicki <cbarwicki14@gmail.com>
2026-09-11 20:45:29 -0400

    next.js updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import Navigation from "./components/Navigation";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./context/AuthContext";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+// export const instant = false;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,10 @@
 // import LoginForm from './LoginForm';
 import LoginForm from "@/app/login/loginForm";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+// export const instant = false;
+
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh]">
